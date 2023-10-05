32729dee63863be1861850c53f196b8e7fe30d1b
Kulpot <kulpotwahu@gmail.com>
2023-10-05 21:10:29 +0800

    Add project files.

---
+++ Visual Studio Code Snippets/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+//ref link:https://www.youtube.com/watch?v=k3VPCamU7V8&list=PLF932FF551E0ADD16&index=10
+//ctrl+k+b -- code snippets manager
+
+class MainClass
+{
+    static void Main()
+    {
+        string[] people = new[] { "Kulpot", "Jim", "Bob" };
+        foreach (string name in people)
+        {
+            Console.WriteLine(name);
+        }
+
+
+        //for (int i = 0; i < length; i++)
+        //{
+        //     
+        //}
+    }
+}
